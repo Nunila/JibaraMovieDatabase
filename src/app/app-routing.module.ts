4a5b8858f3ac950adc6b8c08f8078f063fd21a17
Nunila <nunila.davila@upr.edu>
2018-07-07 12:05:02 +0200

    Added login component in router and added account icon in toolbar. Now need to implement authentication.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { HomeComponent } from './home/home.component';
 import { MoviePreviewComponent } from './movie-preview/movie-preview.component';
 import { SearchMoviesComponent } from './search-movies/search-movies.component';
 import { ModifyDBComponent } from './modify-db/modify-db.component';
+import {SignInComponent} from './sign-in/sign-in.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: SignInComponent},
   { path: 'home', component: HomeComponent },
   { path: 'movie_preview/:id', component: MoviePreviewComponent},
   { path: 'search_movies', component: SearchMoviesComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
-import { MoviesService } from './services/movies-service/movies.service'
-import { SearchMoviesComponent } from './search-movies/search-movies.component';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
@@ -18,13 +14,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu';
 
-
+import { MoviesService } from './services/movies-service/movies.service'
+import { SearchMoviesComponent } from './search-movies/search-movies.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 import { HomeComponent } from './home/home.component';
 import { MoviePreviewComponent } from './movie-preview/movie-preview.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { ModifyDBComponent } from './modify-db/modify-db.component';
-import * as fs from "fs";
 import { SignInComponent } from './sign-in/sign-in.component';
 
 declare var require: any;
@@ -52,6 +50,7 @@ declare var require: any;
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    MatMenuModule,
 
 
     AppRoutingModule
