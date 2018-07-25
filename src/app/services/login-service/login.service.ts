de7adf75c6299f2def1a6cf0d74d4d7b7e1a9a8a
Nunila <nunila.davila@upr.edu>
2018-07-25 18:43:53 +0200

    Pulled branch. Adding more info to movies

--- src/app/services/login-service/login.service.ts
+++ src/app/services/login-service/login.service.ts
@@ -21,9 +21,8 @@ export class LoginService {
   private USERS: Array<User> = [
     {email: 'nunila.davila@upr.edu', username: 'nunila', password: 'admin'},
     {email: 'test@mail.com', username:'elpapisongo', password:'bestnovioever'},
-    {email: '', username:'familia', password:'familia'}
   ]
-  private allowToModify = false;
+  private allowToModify = true;
    
   
 
@@ -42,19 +41,30 @@ export class LoginService {
   }
 
   login(user){
-    if (user.username == 'admin') this.allowToModify = true;
+    if (user.username == 'admin') {
+        this.allowToModify = true;
+        var a = this;
+      this.firebaseDB.ref('/admin/').once('value').then(function(snapshot) {
+      var aa = Object.values(snapshot.val());  
+      firebase.auth().signInWithEmailAndPassword(aa[0], aa[1]).catch(function(error) {
+        console.log(error);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      }); 
+    }); 
+
+    }
     else if (this.USERS.find(uu => {return uu.email == user.username || uu.username == user.username})) {
       this.allowToModify = true;
+      firebase.auth().signInWithEmailAndPassword(user.username, user.password).catch(function(error) {
+        console.log(error);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
     }
     else {
       this.allowToModify = false;
     }
-
-    firebase.auth().signInWithEmailAndPassword(user.username, user.password).catch(function(error) {
-      console.log(error);
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
 
     this.router.navigate([`/home`]);
     
